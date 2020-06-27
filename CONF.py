66223508ac834c58ccefb16fa303e76bb5392ddb
Charubaiel <alexandrin.a@mail.ru>
2020-06-27 16:01:29 +0400

    add conf file

---
+++ CONF.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np 
+import scipy as sp 
+import matplotlib.pyplot as plt 
+import plotly.express as px
+import sklearn
+import time
+from sklearn import *
+import re
+from tqdm import*
+import warnings
+import catboost as ctb
+import lightgbm as lgbm
+warnings.filterwarnings('ignore')
+tqdm.pandas()
+import plotly.figure_factory as ff
+import requests as r
+import grequests as g
+from collections import*
+plt.style.use('ggplot')
